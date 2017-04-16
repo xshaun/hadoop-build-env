82d22375d31c762937d7a3ec2b6da71064bf6ee7
sunxiaoyang <sxy.winner@gmail.com>
2017-04-16 22:41:47 +0800

    Fix: automatically install oracle-java

--- 1.0.pre.requisites.sh
+++ 1.0.pre.requisites.sh
@@ -11,7 +11,9 @@ set -e
 apt-get -y purge openjdk*
 add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
-apt-get -y install oracle-java8-set-default
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get -y install oracle-java8-installer --allow-unauthenticated
+apt-get -y install oracle-java8-set-default --allow-unauthenticated
 apt-get -y install software-properties-common
 # * Basic
 apt-get -y install ssh pdsh curl
