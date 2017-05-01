78b5bac1fc77d093f3f65532225407170059c26d
sunxiaoyang <sxy.winner@gmail.com>
2017-05-01 16:56:26 +0800

    Add script

---
+++ start-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./0.0.user.manual.settings.sh
+./1.0.pre.requisites.sh
+./1.1.switch.ali.maven.sh
+./1.2.settup.passphraseless.sh
+./2.0.run.download.code.sh
+./2.1.run.conf.requisites.sh
+./2.2.run.auxconf.default.xml.sh
+./2.3.run.auxconf.mapred.xml.sh
+./3.0.src.download.code.sh
+./3.1.src.compile.code.sh
+./3.2.src.deploy.code.sh
