b8825aa094a4d282f58624569940e8a93c0388cc
sunxiaoyang <sxy.winner@gmail.com>
2017-05-01 10:20:28 +0800

    Steps 3.2 deploy PSEUDO_DIS_MODE code

---
+++ 3.2.src.deploy.code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+source 0.*
+
+
+if [[ ! -f "${HADOOP_SRC_CODE_PATH}/hadoop-dist/target/hadoop-${HADOOP_VERSION}.tar.gz" ]]; then
+    echo "Can not find 'hadoop-${HADOOP_VERSION}.tar.gz' in '${HADOOP_SRC_CODE_PATH}/hadoop-dist/target/' "
+    echo "Please run 'mvn package -Pdist -DskipTests -Dtar -Dmaven.javadoc.skip=true' in '${HADOOP_SRC_CODE_PATH}' "
+    echo " or run 'mvn package -Pdist,native,docs,src -DskipTests -Dtar' in '${HADOOP_SRC_CODE_PATH}' "
+fi
+
+if [[ 'PSEUDO_DIS_MODE' == ${HADOOP_CLUSTER_MODE} ]]; then
+    tar -zxvf ${HADOOP_SRC_CODE_PATH}/hadoop-dist/target/hadoop-${HADOOP_VERSION}.tar.gz -C ${HADOOP_CODE_LOCATION}
+fi
