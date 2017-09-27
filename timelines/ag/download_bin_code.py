932de73b103b41af7981838ad3d8b4697dd129dc
xshaun <xshaun@outlook.com>
2017-09-27 16:20:04 +0800

    r0.3.1 init

--- timelines/ag/download_bin_code.py
+++ timelines/ag/download_bin_code.py
@@ -31,7 +31,8 @@ class CustomEvent(BasisEvent):
 
         linkaddress = "http://www-eu.apache.org/dist/hadoop/common/hadoop-{0}/hadoop-{0}.tar.gz".format(_version)
         download = "curl -sSL {0} | tar -C {1} -xzv".format(linkaddress, codefolder)
-        movedir = "mv hadoop-{0}/* ./ && rmdir ./hadoop-{0}".format(_version)
+        movedir = "mv {0}* {1} && rmdir {0} ".format(
+            os.path.join(codefolder, "hadoop-%s" % (_version)), codefolder)
 
         retcode = cmd.do("%s && %s" % (download, movedir))
         if retcode != 0:
