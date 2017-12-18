eff132ab03f6d004c7d2c33add5680d06ff06aa9
xshaun <xshaun@outlook.com>
2017-12-18 16:21:41 +0800

    fix - debug

--- utilities/setup_passphraseless.sh
+++ utilities/setup_passphraseless.sh
@@ -22,7 +22,7 @@ ssh_copy_id_auto()
 {
     expect -c "
     set timeout -1;
-    spawn ssh-copy-id '$1';
+    spawn ssh-copy-id $1;
     expect {
         *(yes/no)* {send yes\r; exp_continue;}
         *password:* {send '$2'\r; exp_continue;}
