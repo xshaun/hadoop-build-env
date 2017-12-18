4114f82440f66d17f304591da63bacbe9c96c0b9
xshaun <xshaun@outlook.com>
2017-12-18 17:56:06 +0800

    fix - debug

--- scripts/controlp/configure_site.py
+++ scripts/controlp/configure_site.py
@@ -126,7 +126,7 @@ class Custom(Basis):
                 put_config_line --file {0} --property {3} --value {4} --prefix 'export' && \
                   sleep 0.5".format(
             file,
-            'JAVA_HOME', '`whereis java | awk '{print $3}'`',
+            'JAVA_HOME', "`whereis java | awk '{print $3}'`",
             'PDSH_RCMD_TYPE', 'ssh')
 
         retcode = cmd.do(ins)
