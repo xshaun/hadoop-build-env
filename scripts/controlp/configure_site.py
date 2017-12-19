a5651d8db42a92992bf2ccfb6f271339dc8b8bce
xshaun <xshaun@outlook.com>
2017-12-19 12:22:49 +0800

    fix - debug

--- scripts/controlp/configure_site.py
+++ scripts/controlp/configure_site.py
@@ -34,7 +34,7 @@ def putconfig(file, name, value):
     value_elem.text = value
 
     conf_file = open(file, 'wb')
-    conf_file.write(ElementTree.tostring(root))
+    conf_file.write(ElementTree.tostring(root, pretty_print=True, encoding='utf-8'))
     conf_file.close()
 
 
