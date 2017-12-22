3971412ea467aa0c247b3c1b1a39be7740210239
sunxiaoyang <sxy.winner@gmail.com>
2017-12-22 11:17:48 +0800

    fix maven OutOfMemoryError

--- utilities/install_compilation_prerequisites.sh
+++ utilities/install_compilation_prerequisites.sh
@@ -77,7 +77,8 @@ put_config_line --file ~/.profile --property 'FINDBUGS_HOME' --value '/opt/findb
 
 #
 # * set maven opts
-put_config_line --file ~/.profile --property 'MAVEN_OPTS' --value '"-Xmx1024m -XX:MaxPermSize=128m"' --prefix 'export'
+put_config_line --file ~/.profile --property 'MAVEN_OPTS' --value '"-Xmx1024m"' --prefix 'export'
+put_config_line --file ~/.profile --property 'JAVA_TOOL_OPTIONS' --value '"-Xmx1024m -Xms1024m"' --prefix 'export'
 
 # ----------------------------
 # Clean and Upgrade libs:
