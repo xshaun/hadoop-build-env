bccce6da637bf6ec08702457fa497e3e4f7c851f
xshaun <xshaun@outlook.com>
2017-12-29 22:56:57 +0800

    upgrade version2.1

---
+++ scripts/appexamples/submit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from scripts.basis import Basis
+from scripts.basis import logger
+from scripts.command import Command
+from random import choice
+import os
+
+
+class Custom(Basis):
+
+    def action(self):
+        logger.info('--> common.submit <--')
+
+        ssh_option = '-o StrictHostKeyChecking=no -o ConnectTimeout=600'
+
+        slaves_list = self.getSlaveHosts()
+
+        instructions = list()
+        for p in self.ys['params']:
+            host = choice(slaves_list)
+
+            #!!! donot use -tt option
+            ins = "ssh {0} {2}@{1} -T 'cd {3} && {4}' ".format(
+                ssh_option, host['ip'], host['usr'],
+                self.getClusterBinaryDir(), p)
+
+            instructions.append(ins)
+
+        return Command.parallel(instructions)
+
+def trigger(ys):
+    e = Custom(ys, attempts=3, interval=3, auto=True)
+    return e.status
